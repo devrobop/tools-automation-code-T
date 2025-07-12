4d48bf9df24277125b9a8d892e39bd4da5d588fc
prudhvi-1122 <prudhwi1998@gmail.com>
2025-07-12 20:55:20 +0530

    elk installation

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ module "tool-infra-create" {
   name           = each.key
   instance_type  = each.value["instance_type"]
   policy_name    = each.value["policy_name"]
-#   hosted_zone_id = var.hosted_zone_id
+  hosted_zone_id = var.hosted_zone_id
   ports          = each.value["ports"]
   volume_size    = each.value["volume_size"]
 }
